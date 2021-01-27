d912b641de6ae8eeaf80779e6e7fc3356f6bf3d0
donnchin <donnchin@gmail.com>
2021-01-27 14:39:35 -0500

    modified mysql to ref items in todos DB, and correct server api.js to reflect change

--- routes/api.js
+++ routes/api.js
@@ -15,25 +15,25 @@ router.use(bodyParser.json());
 /*******************Routes**************** */
 
 //Get hompage
-//client request for homepage
+//client request for homepage, Browser will prompt you to got to "/api"
 router.get("/", (req, res) => {
   //Once connected, expects a response with a message
   res.send("Welcome to the API");
 });
 
 //GET all data from DB
-router.get("/todos/items2", async (req, res) => {
-  //Catch errors in case something goes wrong
+//!!!!!!Make sure url matches this!!!!!!!!!!
+router.get("/todos/items", async (req, res) => {
   try {
-    //db data must be known, we select from the table called "items"
-    let results = await db("SELECT * FROM items2 ORDER BY id ASC;");
+    //db data must be known, we select from the table called "items", NOT the DB "todos"
+    let results = await db("SELECT * FROM items ORDER BY id ASC;");
     //check
     console.log("RESULTS", results);
     //you should send back the full list of items with status
     res.status(200).send(results.data);
     //Catch any errors
   } catch (err) {
-    //Response to error, 500 status with message
+    //Response to error, 404 status with message
   }
   res.status(404).send({ error: "List not found." });
 });
@@ -49,14 +49,15 @@ router.get("/todos/items2", async (req, res) => {
 // });
 
 //POST new data, MAKE SURE SOURCE LINK IS SET TO : http://localhost:5000/api/todos/items2
-//NEED TO MAKE SURE MYSQLHAS COLUMN "TASK" BEFORE TESTING
-//Make sure http matches mysql table features, with table name: "items2"(or whatever), and column called : "task"
-router.post("/todos/items2", async (req, res) => {
+//!!NEED TO MAKE SURE MYSQL HAS COLUMN "TASK" BEFORE TESTING
+//Make sure http matches mysql table features, In MYSQL check table name: "items"(or whatever), and column called : "id" & "task"
+//!!!!!!Make sure url matches this!!!!!!!!!!
+router.post("/todos/items", async (req, res) => {
   // The request's body is available in req.body
 
   let { task } = req.body;
   let sql = `
-    INSERT INTO items2 (task)
+    INSERT INTO items (task)
     VALUES ('${task}')
   `;
 
@@ -66,7 +67,7 @@ router.post("/todos/items2", async (req, res) => {
     let results = await db(sql);
 
     //Select all data, // If the query is successful
-    results = await db("SELECT * FROM items2");
+    results = await db("SELECT * FROM items");
 
     //you should send back the full list of items
     //console.log(results.data);
@@ -83,7 +84,7 @@ router.post("/todos/items2", async (req, res) => {
 //PUT data Update/Replace by ID
 // router.put("/todos/:todo_id", old route
 
-router.put("/todos/items2/:id", (req, res) => {
+router.put("/todos/items/:id", (req, res) => {
   // The request's body is available in req.body
   // URL params are available in req.params
   // If the query is successfull you should send back the full list of items
@@ -96,7 +97,7 @@ router.put("/todos/items2/:id", (req, res) => {
 });
 
 //DELETE data by ID
-router.delete("/todos/:todo_id", (req, res) => {
+router.delete("/todos/items/:id", (req, res) => {
   // URL params are available in req.params
   // Add your code here
   //
