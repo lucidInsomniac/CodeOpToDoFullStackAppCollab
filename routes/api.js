f4908a3b73fa596886ce48933d99233720f6cb43
donnchin <donnchin@gmail.com>
2021-01-27 13:47:27 -0500

    Got server end to GET all, GET homepage, and POST from mysql, and postman

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 /*************Boiler plate************ */
 
-//importing necessary components
+//importing necessary components and depenedencies
 var express = require("express");
 var router = express.Router();
 const bodyParser = require("body-parser");
@@ -22,11 +22,11 @@ router.get("/", (req, res) => {
 });
 
 //GET all data from DB
-router.get("/todos", async (req, res) => {
+router.get("/todos/items2", async (req, res) => {
   //Catch errors in case something goes wrong
   try {
     //db data must be known, we select from the table called "items"
-    let results = await db("SELECT * FROM items ORDER BY id ASC;");
+    let results = await db("SELECT * FROM items2 ORDER BY id ASC;");
     //check
     console.log("RESULTS", results);
     //you should send back the full list of items with status
@@ -35,7 +35,7 @@ router.get("/todos", async (req, res) => {
   } catch (err) {
     //Response to error, 500 status with message
   }
-  res.status(500).send({ error: err.message });
+  res.status(404).send({ error: "List not found." });
 });
 
 //      ORIGINAL
@@ -48,33 +48,50 @@ router.get("/todos", async (req, res) => {
 //     .catch(err => res.status(500).send(err));
 // });
 
-//POST new data
-router.post("/todos", (req, res) => {
+//POST new data, MAKE SURE SOURCE LINK IS SET TO : http://localhost:5000/api/todos/items2
+//NEED TO MAKE SURE MYSQLHAS COLUMN "TASK" BEFORE TESTING
+//Make sure http matches mysql table features, with table name: "items2"(or whatever), and column called : "task"
+router.post("/todos/items2", async (req, res) => {
   // The request's body is available in req.body
+
+  let { task } = req.body;
+  let sql = `
+    INSERT INTO items2 (task)
+    VALUES ('${task}')
+  `;
 
   //try
   try {
     //inserts the data
+    let results = await db(sql);
 
     //Select all data, // If the query is successful
+    results = await db("SELECT * FROM items2");
 
     //you should send back the full list of items
+    //console.log(results.data);
     res.status(201).send(results.data);
   } catch (err) {
     //Catch errors if any encountered
     //Response to error, 500 status with message
-    res.status(500).send({ error: err.message });
+    res.status(404).send({ error: err.message });
   }
 });
 
 /***************We will Go over on Thurs************** */
 
 //PUT data Update/Replace by ID
-router.put("/todos/:todo_id", (req, res) => {
+// router.put("/todos/:todo_id", old route
+
+router.put("/todos/items2/:id", (req, res) => {
   // The request's body is available in req.body
   // URL params are available in req.params
   // If the query is successfull you should send back the full list of items
-  // Add your code here
+  // task not found; return 404 status code
+  // Create new obj from request body
+  // Make sure modified task doesn't try to change ID
+  // Replace old task with modified one
+  // Return modified task as confirmation
   //
 });
 
