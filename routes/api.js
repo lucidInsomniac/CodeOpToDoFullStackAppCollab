6a3d295aab3bb666c9b19d3d7f0c888380ae1b2e
donnchin <donnchin@gmail.com>
2021-01-26 22:00:28 -0500

    psuedo code added to server api.js file

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,66 @@
+/*************Boiler plate************ */
+
+//importing necessary components
 var express = require("express");
 var router = express.Router();
 const bodyParser = require("body-parser");
 const db = require("../model/helper");
 
+/*************Middleware*************** */
+
+//This lets the server read JSON from the client
+//request and convers them to JS
 router.use(bodyParser.json());
 
+/*******************Routes**************** */
+
+//Get hompage
+//client request for homepage
 router.get("/", (req, res) => {
+  //Once connected, expects a response with a message
   res.send("Welcome to the API");
 });
 
-router.get("/todos", (req, res) => {
-  // Send back the full list of items
-  db("SELECT * FROM items ORDER BY id ASC;")
-    .then(results => {
-      res.send(results.data);
-    })
-    .catch(err => res.status(500).send(err));
+//GET all data from DB
+router.get("/todos", async (req, res) => {
+  //Catch errors in case something goes wrong
+  try {
+    //db data must be known
+    let results = await db("SELECT * FROM items ORDER BY id ASC;");
+    //check
+    console.log("RESULTS", results);
+    //you should send back the full list of items with status
+    res.status(200).send(results.data);
+    //Catch any errors
+  } catch (err) {
+    //Response to error, 500 status with message
+  }
+  res.status(500).send({ error: err.message });
 });
 
+// router.get("/todos", (req, res) => {
+//   // Send back the full list of items
+//   db("SELECT * FROM items ORDER BY id ASC;")
+//     .then(results => {
+//       res.send(results.data);
+//     })
+//     .catch(err => res.status(500).send(err));
+// });
+
+//POST new data
 router.post("/todos", (req, res) => {
   // The request's body is available in req.body
-  // If the query is successfull you should send back the full list of items
-  // Add your code here
-  //
+  //try
+  //inserts the data
+  //Select all data, // If the query is successful
+  //you should send back the full list of items
+  //Catch errors if any encountered
+  //Response to error, 500 status with message
 });
 
+/***************We will Go over on Thurs************** */
+
+//PUT data Update/Replace by ID
 router.put("/todos/:todo_id", (req, res) => {
   // The request's body is available in req.body
   // URL params are available in req.params
@@ -33,6 +69,7 @@ router.put("/todos/:todo_id", (req, res) => {
   //
 });
 
+//DELETE data by ID
 router.delete("/todos/:todo_id", (req, res) => {
   // URL params are available in req.params
   // Add your code here
